123207c20393fb145020a925e17ba2b7e7949e16
williaamhoang <willhoang1@gmail.com>
2024-11-07 03:46:31 -0600

    Added head.tsx, use client, and some twinkling stars

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+        <>
+            <title>My Portfolio</title>
+            <meta name="description" content="A simple website to hold my portfolio" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    );
+}
